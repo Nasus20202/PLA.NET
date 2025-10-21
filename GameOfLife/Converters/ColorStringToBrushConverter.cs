92b6a48a2b4940ac1b57737b969462f72d54e6d1
Krzysztof Nasuta <krzysztof@nasuta.dev>
2025-10-21 23:54:19 +0200

    fix: custom colors

--- GameOfLife/Converters/ColorStringToBrushConverter.cs
+++ GameOfLife/Converters/ColorStringToBrushConverter.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Globalization;
+﻿using System.Globalization;
 using System.Windows.Data;
 using System.Windows.Media;
 
@@ -10,28 +9,34 @@ namespace GameOfLife.Converters;
 /// </summary>
 public class ColorStringToBrushConverter : IValueConverter
 {
-    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    private static readonly Brush DefaultBrush = Brushes.LimeGreen;
+    private const string DefaultColorString = "#FF00FF00";
+
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
-        if (value is string colorString)
+        if (value is not string colorString || string.IsNullOrWhiteSpace(colorString))
         {
-            try
-            {
-                return (Brush)new BrushConverter().ConvertFrom(colorString)!;
-            }
-            catch
-            {
-                return Brushes.LimeGreen;
-            }
+            return DefaultBrush;
         }
-        return Brushes.LimeGreen;
+
+        try
+        {
+            var converter = new BrushConverter();
+            var result = converter.ConvertFromString(colorString);
+            return result as Brush ?? DefaultBrush;
+        }
+        catch
+        {
+            return DefaultBrush;
+        }
     }
 
-    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
         if (value is SolidColorBrush brush)
         {
             return brush.Color.ToString();
         }
-        return "#FF00FF00";
+        return DefaultColorString;
     }
 }
